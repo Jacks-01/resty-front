1a458c27d1ba924f9bd3f123564cb2297cea33b1
Jack Stubblefield <Jack011601@gmail.com>
2022-10-25 18:09:49 -0500

    refactored footer into a functional component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+export const Footer = (props) => {
+  return <footer>&copy; 2018</footer>;
 }
-
-export default Footer;
