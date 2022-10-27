7214b6e0c84a2802164b4e9a9d6a362580b97dbc
Jack Stubblefield <Jack011601@gmail.com>
2022-10-27 15:30:27 -0500

    added text area

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -21,6 +21,7 @@ const Form = props => {
           <input onChange={(e) => { setUrl(e.target.value) }} placeholder='URL' name='url' type='text' />
           <button type="submit">GO!</button>
         </label>
+        <textarea name='textData' placeholder='json goes here...'></textarea>
         <label className="methods">
           <span onClick={(e) => { setMethod(e.target.innerHTML) }} className='crud' id="get">GET</span>
           <span onClick={(e) => { setMethod(e.target.innerHTML) }} className='crud' id="post">POST</span>
